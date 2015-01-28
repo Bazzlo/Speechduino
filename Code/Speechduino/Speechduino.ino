43c3bcfc9eb40aadf429c1d39f321838c386756e
LaurensKoops <laurens_koops@hotmail.com>
2015-01-28 15:15:01 +0100

    First Release Version
    Release Version 1.0
    
    All working functions remain and developing functions have been removed
    for a clean version.

--- Code/Speechduino/Speechduino.ino
+++ Code/Speechduino/Speechduino.ino
@@ -11,7 +11,7 @@ const int LedRow[] = {36, 34, 32, 30, 47, 49, 51, 53};
 const int grn[] =   {31, 33, 35, 37, 39, 41, 43, 45};
 const int red[] =   {29, 27, 25, 23, 22, 24, 26, 28};
 /***********VARIABLES*************/
-int val, numbers[10], currentNumber, receivedNumbers[8], foodx[10], foody[10], snakex[12], snakey[12], snakelength, snakefield[8][8];
+int val;
 int display_speed = 50;
 int diceArray[5][12] = {
   {1, 3, 6, 4, 2, 3, 5, 1, 2, 6, 5, 4},
@@ -21,10 +21,9 @@ int diceArray[5][12] = {
   {6, 3, 4, 5, 1, 2, 3, 1, 4, 5, 2, 1}
 };
 byte dataType = 0;
-bool sleep, visualize = false;
 char im[128], data[128], data_avgs[14];
 BitVoicerSerial bvSerial = BitVoicerSerial();
-enum Mode { WAKE, SLEEP, EX1, EX2, EX3, RED, GREEN, SIMONSAYS, SNAKE, EIGHTBALL, VISUALIZER, VOLUME, DICE };
+enum Mode { WAKE, SLEEP, EX1, EX2, EX3, RED, GREEN, EIGHTBALL, VISUALIZER, VOLUME, DICE };
 Mode mode;
 /***********METHODS***************/
 void setup()
@@ -49,8 +48,7 @@ void setup()
   Serial1.print('V-40\n');
   delay(10);
   Serial1.flush();
-  //simonSaysRun();
-  example4();
+  speechduinoIntro();
   mode = WAKE;
 }
 
@@ -58,7 +56,7 @@ void loop()
 {
   if (mode == WAKE)
   {
-    //showSprite(happyFace, 100);
+    showSprite(happyFace, 100);
   }
 }
 
@@ -108,14 +106,6 @@ void serialEvent()
         showSprite(greenLeds, 500);
         mode = WAKE;
       }
-      if (data == "SIMONSAYS") {
-        mode = SIMONSAYS;
-        simonSaysRun(0);
-      }
-      if (data == "SNAKE") {
-        mode = SNAKE;
-        snakeRun();
-      }
       if (data == "EIGHTBALL") {
         mode = EIGHTBALL;
         eightBall();
@@ -134,47 +124,6 @@ void serialEvent()
       }
       if (data == "THANKS") {
          speechduinoSleep();
-      }
-      /***********SNAKE*************/
-      if (data == "SNAKE-UP" && mode == SNAKE ) {
-        moveSnake('u');
-      }
-      if (data == "SNAKE-DOWN" && mode == SNAKE) {
-        moveSnake('d');
-      }
-      if (data == "SNAKE-RIGHT" && mode == SNAKE) {
-        moveSnake('r');
-      }
-      if (data == "SNAKE-LEFT" && mode == SNAKE) {
-        moveSnake('l');
-      }
-      /***********SIMONSAYS*************/
-      if (data == "ss1" && mode == SIMONSAYS) {
-        //receivedNumbers = 1;
-      }
-      if (data == "ss2" && mode == SIMONSAYS) {
-        //receivedNumbers = 2;
-      }
-      if (data == "ss3" && mode == SIMONSAYS) {
-        //receivedNumbers = 3;
-      }
-      if (data == "ss4" && mode == SIMONSAYS) {
-        //receivedNumbers = 4;
-      }
-      if (data == "ss5" && mode == SIMONSAYS) {
-        //receivedNumbers = 5;
-      }
-      if (data == "ss6" && mode == SIMONSAYS) {
-        //receivedNumbers = 6;
-      }
-      if (data == "ss7" && mode == SIMONSAYS) {
-        //receivedNumbers = 7;
-      }
-      if (data == "ss8" && mode == SIMONSAYS) {
-        //receivedNumbers = 8;
-      }
-      if (data == "ss9" && mode == SIMONSAYS) {
-        //receivedNumbers = 9;
       }
     }
   }
